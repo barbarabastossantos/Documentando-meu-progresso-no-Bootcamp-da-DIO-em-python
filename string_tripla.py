60b646d0268c099ceb2d549bc5e6892f6f67aaf0
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-03-05 21:25:29 -0300

    multiplas strings

---
+++ string_tripla.py
@@ -0,0 +1,19 @@
+nome = "Barbara"
+
+messagem = f'''
+ Ola, meu nome é {nome},
+   adoro gatos! eles são gostosos!
+Gatos são macios? sim!
+         Gatos são carinhosos.
+amo eles.
+'''
+print(messagem)
+
+
+# Multiplas strings ou string triplas são muito uteis
+# quando vc quer fazer um menu ou uma messagem grande para o seu usuario
+# atribui uma variavel no nosso caso foi nome com valor barbara
+# depois criamos outra variavel a messagem que vai receber a multipla string 
+# messagem recebe f '''  colocamos o texto e fechamos com ''' 
+# por fim mostramos na tela com print
+# perceba que a messagem e imprimida exatamente como escrevemos  na multipla string
