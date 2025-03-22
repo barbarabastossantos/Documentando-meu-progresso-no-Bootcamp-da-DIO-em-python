c773f5e395858b6c278c7a6e657256859028a0e4
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-03-22 20:12:55 -0300

    Listas dentro de listas

---
+++ Lista/listas_aninhadas.py
@@ -0,0 +1,23 @@
+# Listas podem armazenar outras listas
+# Podemos criar estruturas bidimensionais  como tabelas
+# Acessar elementos informando os indices de linha e coluna 
+# Dá pra fazer representação de  Matriz
+
+matriz=[     # <- abrimos uma lista que tem três listas dentro dela
+     [1,"a",2], # linha 0
+     ["b",3,4], # linha 1
+     [6,5,"c"]  # linha 2
+]
+
+print(matriz[0])
+print(matriz[0] [0])
+print(matriz[0] [-1])
+print(matriz[-1] [-1])
+print(matriz[1])
+print(matriz[2])
+print(matriz[1] [-2])
+print(matriz[2] [-3])
+print(matriz[2] [0])
+print(matriz[2] [-2])
+
+# primeiro colcetes e linha o segundo e coluna 
