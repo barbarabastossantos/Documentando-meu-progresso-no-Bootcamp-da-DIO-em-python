94a4cbd397645a9d38d3743114f187ba94f04467
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-03-27 20:37:11 -0300

    unir conjuntos

---
+++ Conjuntos/Metodos_set/union.py
@@ -0,0 +1,8 @@
+# metodo union serve para unir os conjuntos
+
+conjunto_a={1,2}
+conjunto_b={3,4}
+
+print("Conjunto A ", conjunto_a)
+print( "Conjunto B " ,conjunto_b)
+print("A união dos dois conjuntos ", conjunto_a.union(conjunto_b))
