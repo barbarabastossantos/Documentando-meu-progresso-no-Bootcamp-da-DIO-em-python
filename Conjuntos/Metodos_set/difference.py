f3fd952650cfe7b45935468551a8f2cd3943fdfe
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-03-28 06:03:03 -0300

    O elemento que so aparece em um dos conjuntos

---
+++ Conjuntos/Metodos_set/difference.py
@@ -0,0 +1,8 @@
+#pega a parte que so tem em umconjunto( o metodo difference e o oposto do metodo intersection)
+
+conjunto_a={1,2,3}
+conjunto_b={2,3,4}
+print("Conjunto A" , conjunto_a)
+print("Conjunto B", conjunto_b)
+print("O elemento diferente no conjunto A ", conjunto_a.difference(conjunto_b))
+print("O elemento diferente no conjunto B ", conjunto_b.difference(conjunto_a))
