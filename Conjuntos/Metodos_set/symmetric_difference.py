403381d2e85f2a8b51319b1d274f44e328fad40b
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-03-28 06:10:39 -0300

    elementos que não fazem parte da interserção

---
+++ Conjuntos/Metodos_set/symmetric_difference.py
@@ -0,0 +1,7 @@
+# Todos os elementos que não estão na interserção
+
+conjunto_a={1,2,3}
+conjunto_b={2,3,4}
+print("Conjunto A ", conjunto_a)
+print("Conjunto B ", conjunto_b)
+print("Os elementos de A e B que não fazem parte da interserção são : " ,conjunto_a.symmetric_difference(conjunto_b))
