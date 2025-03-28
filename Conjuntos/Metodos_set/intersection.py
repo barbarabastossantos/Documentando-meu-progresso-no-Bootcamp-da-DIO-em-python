9792a92568853990ecc909936f92d14ec8981a26
Barbara Bastos <barbara.bastos1995@gmail.com>
2025-03-28 05:48:44 -0300

    elementos em ambos os conjuntos, elementos em comum

---
+++ Conjuntos/Metodos_set/intersection.py
@@ -0,0 +1,8 @@
+# retorna os elementos que estão em ambos os conjuntos, a interserção
+
+conjunto_a={1,2,3}
+conjunto_b={2,3,4}
+
+print("Conjunto A ", conjunto_a)
+print("Conjunto B ", conjunto_b)
+print("Essa é a intercerssão entre A e B ", conjunto_a.intersection(conjunto_b))
